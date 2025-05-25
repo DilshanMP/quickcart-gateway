8f256451c8a1c425617aabe42a54cf1da257b48f
DilshanMP <mpdilshan.99@gmail.com>
2025-05-25 17:58:37 +0530

    quick cart gateway config

---
+++ src/main/java/com/dilshandev/quickcart/quickcart_gateway/config/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.dilshandev.quickcart.quickcart_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+
+                .route("quick-cart-user-service-api", r->r.path("user-service/**")
+                        .uri("http://localhost:8082")).build();
+
+    }
+
+
+}
---
+++ src/test/java/com/dilshandev/quickcart/quickcart_gateway/QuickcartGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilshandev.quickcart.quickcart_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcartGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
